13b171cf6c70768d04cfc5d177b57652180f48db
Mihir Patel <mihirkpatel9702@gmail.com>
2022-02-01 16:15:32 -0500

    done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const hbs = require('hbs');
 const path = require('path');
@@ -12,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -21,5 +23,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(response => res.render('beers', { beers: response })) 
+    .catch(err => console.log(err))
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beers => res.render('random-beer', { beer:
+    { name: beers[0].name,
+      id: beers[0].id,
+      tagline: beers[0].tagline,
+      image_url: beers[0].image_url,
+      description: beers[0].description,
+      brewerTips: beers[0].brewers_tips,
+      foodPairing: beers[0].food_pairing }
+    })) 
+    .catch(err => console.log(err))
+});
+
+app.get('/beers/beer-:anything', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(idResponse => res.render('beerid', {
+       id: req.params.anything,
+       name: idResponse[req.params.anything-1].name,
+       description: idResponse[req.params.anything-1].description,
+       firstBrew: idResponse[req.params.anything-1].first_brewed,
+       volume: idResponse[req.params.anything-1].volume.value
+       }))
+    .catch(err => console.log(err))
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send('404 Page not found');
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
